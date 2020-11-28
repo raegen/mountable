9a8a595b7099ce305730ec47c8f845427afca8e4
Nikola Pavlovic <npavlovic101@gmail.com>
2020-11-28 17:08:17 +0100

    fucking update isMountable

--- projects/mountable/src/lib/util.ts
+++ projects/mountable/src/lib/util.ts
@@ -4,6 +4,6 @@ import {Mountable} from "./mountable.decorator";
 export const NG_MNT_DEF = 'ɵmnt';
 export const MOUNTABLE_KEY_NAME = '__ngMountable__';
 
-export const isMountable = (target: Type<any> | any) => NG_MNT_DEF in (typeof target === 'function' ? target : target?.constructor);
+export const isMountable = (target: Type<any> | any) => target && NG_MNT_DEF in (typeof target === 'function' ? target : target.constructor);
 
 export const getMountable = (target: Mountable) => target?.[MOUNTABLE_KEY_NAME];
