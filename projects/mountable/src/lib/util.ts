e3acda00672b360ca30233b97463f6be57cc6f62
Nikola Pavlovic <npavlovic101@gmail.com>
2020-11-28 17:35:30 +0100

    fucking update types again

--- projects/mountable/src/lib/util.ts
+++ projects/mountable/src/lib/util.ts
@@ -1,9 +1,8 @@
 import {Type} from "@angular/core";
-import {Mounter} from "./mounter.service";
 
 export const NG_MNT_DEF = 'ɵmnt';
 export const MOUNTABLE_KEY_NAME = '__ngMountable__';
 
 export const isMountable = (target: Type<any> | any) => target && NG_MNT_DEF in (typeof target === 'function' ? target : target.constructor);
 
-export const getMountable = <T extends {[MOUNTABLE_KEY_NAME]?: Mounter}>(target: T) => target?.[MOUNTABLE_KEY_NAME];
+export const getMountable = (target: any) => target?.[MOUNTABLE_KEY_NAME];
