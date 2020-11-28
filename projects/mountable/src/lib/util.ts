69f15dc5dbab6997322ad869916f04e14cda93af
Nikola Pavlovic <npavlovic101@gmail.com>
2020-11-28 17:29:40 +0100

    update types

--- projects/mountable/src/lib/util.ts
+++ projects/mountable/src/lib/util.ts
@@ -6,4 +6,4 @@ export const MOUNTABLE_KEY_NAME = '__ngMountable__';
 
 export const isMountable = (target: Type<any> | any) => target && NG_MNT_DEF in (typeof target === 'function' ? target : target.constructor);
 
-export const getMountable = (target: {[MOUNTABLE_KEY_NAME]?: Mounter}) => target?.[MOUNTABLE_KEY_NAME];
+export const getMountable = <T extends {[MOUNTABLE_KEY_NAME]?: Mounter}>(target: T) => target?.[MOUNTABLE_KEY_NAME];
